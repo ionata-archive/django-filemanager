212bcedf136b40078c84d619d1b0f7505dffffaf
Tim Heap <tim@ionata.com.au>
2013-10-29 14:58:20 +1100

    Add file browser widget/model field

--- filemanager/models.py
+++ filemanager/models.py
@@ -1,3 +1,24 @@
-from django.db import models
+from filemanager import fields
+from django.db.models import FileField
 
-# Create your models here.
+
+class FileBrowserField(FileField):
+    def formfield(self, **kwargs):
+        defaults = {
+            'form_class': fields.FileBrowserField,
+        }
+        defaults.update(kwargs)
+        return super(FileBrowserField, self).formfield(**defaults)
+
+    def pre_save(self, model_instance, add):
+        # Skip the pre_save of the FileField, as that tries to save the file
+        # again, resulting in duplicates
+        f = super(FileField, self).pre_save(model_instance, add)
+        return f
+
+
+try:
+    from south.modelsinspector import add_introspection_rules
+    add_introspection_rules([], ["^filemanager\.models\.FileBrowserField"])
+except ImportError:
+    pass
