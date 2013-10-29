ad1d4b8856821827340f9136647a61dec3d8d6c8
Tim Heap <tim@ionata.com.au>
2013-10-29 17:07:23 +1100

    Version bump to 0.2.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except ImportError:
 
 setup(
     name='django-filemanager',
-    version="0.2.0",
+    version="0.2.1",
     description='A django filemanager app',
     author='Ionata Web Solutions',
     author_email='webmaster@ionata.com.au',
